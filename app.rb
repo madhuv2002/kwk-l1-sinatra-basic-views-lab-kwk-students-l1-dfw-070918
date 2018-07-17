7641d3ae049fcb0647b59230cc119b40742c4847
madhuv2002 <madhuv2002@gmail.com>
2018-07-17 18:38:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,5 +4,12 @@ class App < Sinatra::Base
   get '/bowling-name' do
     "<h1> Buster's Best Bowling Alley</h1>"
   end
+  
+  get "/bowling-tagline" do
+    "<h2> Bet You Can't Bowl A Strike Every Time</h2>"
+  end
+  
+  get "/bowling-address" do
+    
 
 end
